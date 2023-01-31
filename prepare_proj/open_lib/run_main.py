ad3841a1c6b17eb3ca91820d9d7bf4ada8f42500
kr-MATAGI <dev_matagi@kakao.com>
2023-01-31 13:48:29 +0900

    from import modify

--- prepare_proj/open_lib/run_main.py
+++ prepare_proj/open_lib/run_main.py
@@ -1,4 +1,4 @@
-from prepare_proj.open_lib.smart_g2p.trans import sentranslit as trans
+from smart_g2p.trans import sentranslit as trans
 
 if "__main__" == __name__:
     trans('1999년 8월29일은 john가 mary을 만난 날로 매년 3시15분 방 3-147에서 의식이 거행된다', if_num=False, if_sym=False)
