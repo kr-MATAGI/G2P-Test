62d9d9d1d7175062d088ef86026e4bd51b14a501
kr-MATAGI <dev_matagi@kakao.com>
2023-01-31 14:41:50 +0900

    proro test on linux env

--- prepare_proj/open_lib/run_pororo.py
+++ prepare_proj/open_lib/run_pororo.py
@@ -4,5 +4,5 @@ from pororo import Pororo
 ### MAIN ###
 if "__main__" == __name__:
     print("[run_pororo][__main__]")
-    g2p = Pororo(task="g2p", lang="kr")
+    g2p = Pororo(task="g2p", lang="kr") # pororo도 g2pk 씀
     print(g2p("1999년 8월29일은 john가 mary을 만난 날로 매년 3시15분 방 3-147에서 의식이 거행된다"))
