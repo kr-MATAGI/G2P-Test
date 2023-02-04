7cddc594918bca1b93b11388e7cfa8fe1e1ab203
kr-MATAGI <dev_matagi@kakao.com>
2023-02-04 16:13:55 +0900

    fixed sigmorphon prons

--- prepare_proj/run_kr_g2p_test.py
+++ prepare_proj/run_kr_g2p_test.py
@@ -1,5 +1,6 @@
 import datasets
 import pandas as pd
+import copy
 from datasets import Dataset
 import evaluate
 
@@ -35,10 +36,15 @@ def run_test_g2pk(target_dataset: Dataset, debug_mode: bool = False):
     for idx, item in enumerate(target_dataset):
         word = item["word"]
         ipa = item["ipa"]
-        pron = item["pron"]
+        pron = item["pron"].split(", ")
 
         res_g2pk = g2pk(word)
         print(f"[run_test_g2pk] {idx}, {res_g2pk}, {ipa}, {pron}")
+
+        if res_g2pk in pron:
+            pron = res_g2pk
+        else:
+            pron = pron[0]
 
         wer_ans_list.append(pron)
         wer_pred_list.append(res_g2pk)
@@ -85,10 +91,15 @@ def run_test_ko_g2p_advanced(target_dataset: Dataset):
     for idx, item in enumerate(target_dataset):
         word = item["word"]
         ipa = item["ipa"]
-        pron = item["pron"]
+        pron = item["pron"].split(", ")
 
         res_kog2p_advanced = KoG2Padvanced(word)
         print(f"[run_test_ko_g2p_advanced] {idx}, {res_kog2p_advanced}, {ipa}, {pron}")
+
+        if res_kog2p_advanced in pron:
+            pron = res_kog2p_advanced
+        else:
+            pron = pron[0]
 
         wer_ans_list.append(pron)
         wer_pred_list.append(res_kog2p_advanced)
@@ -128,10 +139,15 @@ def run_test_smart_g2p(target_dataset: Dataset):
     for idx, item in enumerate(target_dataset):
         word = item["word"]
         ipa = item["ipa"]
-        pron = item["pron"]
+        pron = item["pron"].split(", ")
 
         res_smart_g2p = g2pk(trans(word))
         print(f"[run_test_smart_g2p] {idx}, {res_smart_g2p}, {ipa}, {pron}")
+
+        if res_smart_g2p in pron:
+            pron = res_smart_g2p
+        else:
+            pron = pron[0]
 
         wer_ans_list.append(pron)
         wer_pred_list.append(res_smart_g2p)
@@ -185,13 +201,18 @@ def run_test_g2p(target_dataset: Dataset, rule_book_path: str):
     for idx, item in enumerate(target_dataset):
         word = item["word"]
         ipa = item["ipa"]
-        pron = item["pron"]
+        pron = item["pron"].split(", ")
 
         res_smart_g2p = runKoG2P(word, rule_book_path)
         res_smart_g2p = res_smart_g2p.split(" ")
         res_smart_g2p = "".join([sym_jamo_dict[x] for x in res_smart_g2p])
         res_smart_g2p = join_jamos(res_smart_g2p)
         print(f"[run_test_g2p] {idx}, {res_smart_g2p}, {ipa}, {pron}")
+
+        if res_smart_g2p in pron:
+            pron = res_smart_g2p
+        else:
+            pron = pron[0]
 
         wer_ans_list.append(pron)
         wer_pred_list.append(res_smart_g2p)
@@ -215,33 +236,26 @@ def run_test_g2p(target_dataset: Dataset, rule_book_path: str):
     return {"wer": wer_score, "per": per_score}
 
 #===================================
-def run_pnu_pron(target_dataset: Dataset, compare_target: str = "ipa",
-                 debug_mode: bool = False):
+def run_pnu_pron(target_dataset: Dataset, debug_mode: bool = False):
 #===================================
     print(f"[run_pnu_pron] dataset.size: {len(test_dataset)}")
 
     # open preds
-    all_words = []
+    pnu_preds = []
     with open("data/kr/save_pnu_preds_1.txt", mode="r", encoding="utf-8") as f:
         read_words = f.readlines()
         read_words = [x.replace("\n", "") for x in read_words]
         print(f"[run_pnu_pron] preds_1.size {len(read_words)}")
-        all_words.extend(read_words)
+        pnu_preds.extend(read_words)
     with open("data/kr/save_pnu_preds_2.txt", mode="r", encoding="utf-8") as f:
         read_words = f.readlines()
         read_words = [x.replace("\n", "") for x in read_words]
         print(f"[run_pnu_pron] preds_2.size: {len(read_words)}")
-        all_words.extend(read_words)
-    print(f"[run_pnu_pron] all_words.size: {len(all_words)}")
+        pnu_preds.extend(read_words)
+    print(f"[run_pnu_pron] all_words.size: {len(pnu_preds)}")
 
     # remove space
-    all_words = [x.replace(" ", "") for x in all_words]
-
-    print(all_words)
-    if "ipa" == compare_target:
-        all_words = [x.split("\t")[-1] for x in all_words]
-    else:
-        all_words = [x.split("\t")[-2] for x in all_words]
+    pnu_preds = [x.replace(" ", "").split("\t")[-2] for x in pnu_preds]
 
     # make ans
     wer_ans_list = []
@@ -250,41 +264,29 @@ def run_pnu_pron(target_dataset: Dataset, compare_target: str = "ipa",
     wer_pred_list = []
     per_pred_list = []
 
+    correct_word = []
     in_correct_word = []
-    for idx, (ans, pred) in enumerate(zip(target_dataset, all_words)):
+    for idx, (ans, pred) in enumerate(zip(target_dataset, pnu_preds)):
         word = ans["word"]
         ipa = ans["ipa"]
-        pron = ans["pron"]
+        pron = ans["pron"].split(", ")
 
-        if "ipa" == compare_target:
-            pred = pred.split(", ")
-            print(ipa, pred)
-            input()
-            if ipa in pred:
-                wer_pred_list.append(ipa)
-                per_pred_list.append(ipa)
-            else:
-                wer_pred_list.append(pred[0])
-                per_pred_list.append(pred[0])
-            wer_ans_list.append(ipa)
-            per_ans_list.append(ipa)
+        print(f"[run_pnu_pron] {idx}, {pred}, {ipa}, {pron}")
+        if pred in pron:
+            pron = pred
         else:
-            wer_ans_list.append(pron)
-            jamo_ans = j2hcj(h2j(pron))
-            per_ans_list.append(jamo_ans)
+            pron = pron[0]
 
-        if "ipa" == compare_target:
-            if ipa != pred:
-                in_correct_word.append((str(idx), word, ipa, pron, pred))
+        wer_ans_list.append(pron)
+        wer_pred_list.append(pred)
+
+        per_pred_list.append(j2hcj(h2j(pred)))
+        per_ans_list.append(j2hcj(h2j(pron)))
+
+        if pron != pred:
+            in_correct_word.append((str(idx), word, ipa, pron, pred))
         else:
-            if pron != pred:
-                in_correct_word.append((str(idx), word, ipa, pron, pred))
-
-    if "ipa" != compare_target:
-        per_pred_list = []
-        for pred in all_words:
-            jamo_pred = j2hcj(h2j(pred))
-            per_pred_list.append(jamo_pred)
+            correct_word.append((str(idx), word, ipa, pron))
 
     wer_score = wer_metric.compute(predictions=wer_pred_list, references=wer_ans_list)
     per_score = per_metric.compute(predictions=per_pred_list, references=per_ans_list)
@@ -296,7 +298,7 @@ def run_pnu_pron(target_dataset: Dataset, compare_target: str = "ipa",
             f.write("Index\tword\tipa\tpron\tpred\n")
             for item in in_correct_word:
                 f.write(item[0]+"\t"+item[1]+"\t"+item[2]+"\t"+item[3]+"\t"+item[4]+"\n")
-        print(f"[run_test_g2p] Complete debug mode !")
+        print(f"[run_pnu_pron] Complete debug mode !")
 
     return {"wer": wer_score, "per": per_score}
 
@@ -326,6 +328,46 @@ def compare_sentence_results(sentence: str, rule_book_path: str):
     for k, v in results.items():
         print(f"[compare_sentence_results] {k} : {v}")
 
+#===================================
+def fix_sigmorphon_pron(fixed_list_path: str, target_data_set: Dataset):
+#===================================
+    print(f"[fix_sigmorphon_pron] fixed_list_path: {fixed_list_path}")
+
+    # Read fixed list
+    fixed_list = [] # [ (index, word, fixed_pron) ]
+    with open(fixed_list_path, mode="r", encoding="utf-8") as f:
+        lines = f.readlines()
+        for line in lines:
+            sp_line = line.replace("\n", "").split("\t")
+            fixed_list.append((sp_line[0], sp_line[1], sp_line[-1]))
+        print(f"[fix_sigmorphon_pron] fixed_list.size: {len(fixed_list)}")
+
+    change_cnt = 0
+    word_list = []
+    ipa_list = []
+    pron_list = []
+    for idx, item in enumerate(target_data_set):
+        word = item["word"]
+        ipa = item["ipa"]
+        pron = item["pron"]
+        for fix in fixed_list:
+            if idx == int(fix[0]) and item["word"] == fix[1]:
+                change_cnt += 1
+                pron = fix[-1]
+                break
+        word_list.append(word)
+        ipa_list.append(ipa)
+        pron_list.append(pron)
+
+    new_data = pd.DataFrame()
+    new_data["word"] = word_list
+    new_data["ipa"] = ipa_list
+    new_data["pron"] = pron_list
+
+    print(f"[fix_sigmorphon_pron] change_cnt : {change_cnt}")
+    print(f"[fix_sigmorphon_pron] new_data.size: {len(new_data)}")
+    return Dataset.from_pandas(new_data)
+
 ### MAIN ###
 if "__main__" == __name__:
     print("[run_kr_g2p_test][__main__]")
@@ -337,17 +379,19 @@ if "__main__" == __name__:
     dev_dataset = sig_parser.sig_proun_data_load(target_lang="kor", mode="dev")
     test_dataset = sig_parser.sig_proun_data_load(target_lang="kor", mode="test")
 
+    test_dataset = fix_sigmorphon_pron("./data/kr/sigmorphon/sigmorphon_fixed_list.txt", target_data_set=test_dataset)
+
     print(f"[run_kr_g2p_test][__main__] dataset.size - train: {len(train_dataset)}, "
           f"dev: {len(dev_dataset)}, test: {len(test_dataset)}")
 
     results = {}
-    running_method = ["PNU"]
+    running_method = ["g2pk", "KoG2Padvanced", "SMART", "G2P", "PNU"]
 
     compare_sentence_results("이상한 신을 신고 가는 특이한 사람을 신고했다.", rule_book_path=rule_book_path)
 
     # Run g2pk
     if "g2pk" in running_method:
-        g2pk_score = run_test_g2pk(target_dataset=test_dataset, debug_mode=True)
+        g2pk_score = run_test_g2pk(target_dataset=test_dataset, debug_mode=False)
         results.update({"g2pk": g2pk_score})
 
     # Run KoG2Padvanced
@@ -367,7 +411,8 @@ if "__main__" == __name__:
         results.update({"g2p": g2p_score})
 
     if "PNU" in running_method:
-        pnu_score = run_pnu_pron(test_dataset, debug_mode=True, compare_target="ipa")
+        pnu_score = run_pnu_pron(test_dataset, debug_mode=False)
+        results.update({"pnu": pnu_score})
 
     print("---------------------------------")
     print("[run_kr_g2p_test][__main__] Total Results: ")
