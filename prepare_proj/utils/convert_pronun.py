5165c25b796053ccbc0968f559aa60569b9d92c8
kr-MATAGI <dev_matagi@kakao.com>
2023-02-06 20:35:10 +0900

    단어 단위로 ipa를 국문으로 변환하는 코드

--- prepare_proj/utils/convert_pronun.py
+++ prepare_proj/utils/convert_pronun.py
@@ -85,16 +85,79 @@ class ConvertPronun():
             result_list.append(join_jamos(result))
         return result_list
 
+    def conv_ipa2kor_unit_sent(self, origin: str, ipa: str):
+        mapping = pd.read_csv("../data/kr/dictionary.txt", header=None, delimiter="\t")
+        self.dic = {}
+
+        # mapping dicttionary to dict
+        for i in range(len(mapping)):
+            if mapping[0][i] not in self.dic:
+                self.dic[mapping[0][i]] = mapping[1][i]
+        print(self.dic)
+
+        conv_ipa = re.sub(r"[ ːˈˌ]", "", ipa)
+
+        # ipa -> 발음대로
+        for ph, gr in self.dic.items():
+            conv_ipa = conv_ipa.replace(ph, gr)
+
+        # ipa가 w e̞일 경우 e로 변경
+        exp = "e"
+        origin_we_list = []
+
+        if exp in conv_ipa:
+            # 원래 단어 자모로 분해
+            origin_jamo = split_syllables(origin)
+            for j in range(len(origin_jamo)):
+                if 'ㅚ' in origin_jamo[j]:
+                    origin_we_list.append('ㅚ')
+                elif 'ㅞ' in origin_jamo[j]:
+                    origin_we_list.append('ㅞ')
+                else:
+                    continue
+            # e -> ㅚ or ㅞ
+            print(origin_we_list)
+            for we in origin_we_list:
+                conv_ipa = conv_ipa.replace(exp, we, 1)
+
+        # concat_jamo
+        vowel = ["ㅏ", "ㅓ", "ㅗ", "ㅜ", "ㅡ", "ㅐ", "ㅒ", "ㅘ", "ㅚ", "ㅙ", "ㅢ", "ㅑ", "ㅕ", "ㅛ", "ㅠ", "ㅣ", "ㅔ", "ㅖ", "ㅝ", "ㅟ",
+                 "ㅞ"]
+
+        result_list = []
+        word_list = list(conv_ipa)  # 한 글자씩 분해
+        index = 0
+        for i in range(len(word_list)):
+            # 첫 글자가 모음일 경우
+            if i == 0 and conv_ipa[i] in vowel:
+                word_list.insert(0, "ㅇ")
+                index += 1
+            else:
+                # 전 글자가 이응으로 시작하거나 모음만 있는 경우 이응 추가
+                if (conv_ipa[i] in vowel and conv_ipa[i - 1] == "ㅇ") or (conv_ipa[i] in vowel and conv_ipa[i - 1] in vowel):
+                    word_list.insert(index, "ㅇ")
+                    index += 1
+            index += 1
+        result = "".join(word_list)
+
+        # 자모 합치기
+        result = join_jamos(result)
+        print(result)
+        return result
+
 ### Main ###
 if __name__ == "__main__":
     pronun = ConvertPronun(src_dir="../data/kr/sigmorphon")
 
     for file in pronun.data_list:
+        break
         pronun.read_file(file=file)
         # ㅚ, ㅞ 처리
         pronun.df['pronun'] = pronun.handle_exp(pronun.df[0], pronun.df['pronun'])
+
         # 자음만 있는 경우 이응 추가와 분리된 자모 합치기
         pronun.df['pronun'] = pronun.concat_jamo(pronun.df['pronun'])
 
         # save
         pronun.df.to_csv("../data/kr/pronunciation/pronunciation_"+file, header=None, index=False, sep="\t")
+    pronun.conv_ipa2kor_unit_sent(origin="이제", ipa="iʣe")
